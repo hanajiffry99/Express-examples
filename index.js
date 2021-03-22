e7d095a743c9c0cc6529be7b17dc449f2896b2e7
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-22 17:30:57 +0530

    saving static files

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require ('express');
 const http = require ('http');
+const morgan = require('morgan');
 
 const hostname ='localhost';
 const port = 3000;
 
 const app = express(); // uses express for this application
+app.use(morgan('dev'));//using morgan for development
+
+app.use(express.static(__dirname + '/Public'));
 
 app.use((req,res,next) =>{
-console.log(req.headers);
 res.statusCode =200;
 res.setHeader('content-type','text/html');
 res.end("This is an express server");
@@ -15,5 +18,5 @@ res.end("This is an express server");
 
 const server = http.createServer(app);
 server.listen(port,hostname, ()=>{
-    console.log("the server is running in http://${hostname}:${port}")
-})
+    console.log('the server is running at http://${hostname}:${port}/');
+});
