7c510d0dbde364c073480795fd89c7317e5b8851
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-22 21:26:34 +0530

    rest API

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require ('express');
 const http = require ('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname ='localhost';
 const port = 3000;
 
 const app = express(); // uses express for this application
 app.use(morgan('dev'));//using morgan for development
+app.use(bodyParser.json());//allows to body of req msg that is in json format
+
+app.all('/dishes',(req,res,next) =>{
+    res.statusCode=200;
+    res.setHeader('content-type','text/plain');
+    next();//looks for additional specifications which will matches dishes end point
+});//dishes is the end point,callback function
+
+
+app.get('./dishes',(req,res,next) =>{
+  res.end('will send details of  the dishes' + req.params.dishId + 'to you');
+});
+
+app.post('./dishes',(req,res,next) =>{
+    res.statusCode = 403;
+    res.end('POSt operation not supported on /dishes' + req.params.dishId);
+  });
+
+
+app.put('./dishes',(req,res,next) =>{
+    res.write('updating the dish: ' + req.params.dishId);
+    res.end('will update the dish' + req.body.name + '\n' +'with deatails: ' +  req.body.description );
+  });
+
+
+app.delete('./dishes',(req,res,next) =>{
+    res.end('deleting dishes' + req.params.dishId);
+  });
+
+
+  app.get('./dishes/:dishId',(req,res,next) =>{
+    res.end('will send all the dishes to you');
+  });
+  
+  app.post('./dishes/:dishId',(req,res,next) =>{
+      res.end('will add the dish : ' + req.body.name +  'with details :' + req.body.description);//extract the info from the body that is going to be returned
+    });
+  
+  
+  app.put('./dishes/:dishId',(req,res,next) =>{
+      res.statusCode=403;
+      res.end('PUT operation not supported on /dishes');
+    });
+  
+  
+  app.delete('./dishes/:dishId',(req,res,next) =>{
+      res.end('deleting all the dishes');
+    });
+  
 
 app.use(express.static(__dirname + '/Public'));
 
